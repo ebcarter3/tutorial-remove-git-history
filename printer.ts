3b5746ea24ce82bbbd19c777e6d62608e15fa833
Blair Carter <blair.carter@asurion.com>
2021-10-13 18:28:21 -0500

    initializing

---
+++ printer.ts
@@ -0,0 +1,13 @@
+import {readFileSync} from 'fs'
+
+const user = '{"name": "test name", "dob": "9999-99-99"}'
+
+function readUsers (userFile) {
+    return readFileSync(userFile)
+}
+
+function printUser (users) {
+    console.log(users)
+}
+
+printUser(user)
